fb9c1320ad13204c36c62413d11ed64c768a7e64
Tsunaa-Alexis <alexisottavi@gmail.com>
2022-05-05 15:30:01 +0200

    ajout listing bouton

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 
 $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT']."/Burger";
+include('fonctions/main.php');
 
+//CONFIGURATION LISTING RESULTATS
+$config_resultsParPageDefaut = 20;
+$config_resultsParPage = array(10,20,30,50,100,150,1000);
 
 if(!isset($_GET['action'])){ $_GET['action'] = ""; include('home.php'); }
 if($_GET['action'] === 'inscription'){ include('inscription.php'); }
 if($_GET['action'] === 'login'){ include('login.php');}
 if($_GET['action'] === 'composition'){ include('./modules/composition/composition.php'); }
+if($_GET['action'] === 'listeIngredients'){ include('./modules/ingredients/ingredient.php'); }
 
 
 ?>
